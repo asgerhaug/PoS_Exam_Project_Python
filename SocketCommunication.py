8859d55f2ec40dffcda31709ed1fceaa13b0a5f3
asger <acsc@itu.dk>
2021-05-25 21:11:17 +0200

    p2p tested in Main -working

--- SocketCommunication.py
+++ SocketCommunication.py
@@ -7,3 +7,17 @@ class SocketCommunication(Node):
 
     def startSocketCommunication(self):
         self.start()
+
+    #when a node connects to you, you get this message"
+    def inbound_node_connected(self, connected_node):
+        print('indbound connection')
+        self.send_to_node(connected_node, 'Hi I am the node you connected to')
+
+    #when you connect to a node you get this message"
+    def outbound_node_connected(self, connected_node):
+        print('outbound connection')
+        self.send_to_node(connected_node, 'Hi I am the node who initialized the connection')
+
+    #when you get an incomming message from a node, this method is called
+    def node_message(self, connected_node, message):
+        print(message)
